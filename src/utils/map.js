95b7f277995cb7b325663059d34815104f7d74ba
teejale <jasmine.leivo95@gmail.com>
2026-04-07 12:05:10 +0200

    add user position + marker

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,8 +1,58 @@
-export let map = L.map('map').setView([59.325441, 18.071614], 13);
+export let map = L.map('map');
+map.setView([59.325441, 18.071614], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+
+
+//user navigation 
+
+navigator.geolocation.watchPosition(success, error);
+
+let marker, circle, zoomed; 
+
+// circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
+
+function success(pos) {
+    console.log(pos);
+    const lat = pos.coords.latitude; 
+    const lng = pos.coords.longitude; 
+    const accuracy = pos.coords.accuracy;
+
+    if(marker) {
+        map.removeLayer(marker);
+        map.removeLayer(circle);
+    }
+
+    marker = L.marker([lat, lng]).addTo(map);
+    circle = L.circle([lat, lng], {
+        radius: accuracy,
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+    }).addTo(map);
+
+    if(!zoomed) {
+        zoomed = map.fitBounds(circle.getBounds());
+    } 
+
+    map.setView([lat, lng]);
+}
+
+function error(err) {
+    if(err.code === 1) {
+        alert("Accept Plot Twist to access your current location")
+    } else {
+        alert("Cannot get current location")
+    }
+}
 
