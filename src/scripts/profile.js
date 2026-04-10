e85edaf6a0f4d7efb3c5484ab79066e758e25b4f
emaniooo <eman.abubeker@yh.nackademin.se>
2026-04-10 19:18:32 +0200

    added open and close dropdown function

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -19,15 +19,28 @@ document.addEventListener("DOMContentLoaded", () => {
     sessionStorage.removeItem("loggedIn");
     window.location.href = "index.html";
   });
-    // Visa profil-ikon när man är inloggad
+
+  // Visa profil-ikon när man är inloggad
 const profileIconContainer = document.getElementById("profile-icon-container");
 const profileDropdown = document.getElementById("profile-dropdown");
 const profileIcon = document.getElementById("profile-icon");
-
 profileIconContainer.style.display = "block";
 
 // Användardata
 document.getElementById("dd-name").textContent = user.name;
 document.getElementById("dd-email").textContent = user.email;
+
+// Öppna dropdown
+profileIcon.addEventListener("click", () => {
+  profileDropdown.style.display =
+    profileDropdown.style.display === "block" ? "none" : "block";
+});
+
+// Stäng dropdown
+document.addEventListener("click", (e) => {
+  if (!profileIconContainer.contains(e.target)) {
+    profileDropdown.style.display = "none";
+  }
+});
 
 });
