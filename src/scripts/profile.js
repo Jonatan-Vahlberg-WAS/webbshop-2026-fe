28c359869532983e864e15c5c2b8fc1b1c35b945
emaniooo <eman.abubeker@yh.nackademin.se>
2026-04-10 23:16:41 +0200

    added function to tab

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -59,4 +59,19 @@ document.addEventListener("click", (e) => {
       </div>
     `).join("");
 
+  // Tabs
+  const tabButtons = document.querySelectorAll(".tab-btn");
+  const tabContents = document.querySelectorAll(".tab-content");
+
+  tabButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const tab = btn.dataset.tab;
+
+      tabButtons.forEach(b => b.classList.remove("active"));
+      tabContents.forEach(c => c.classList.remove("active"));
+
+      btn.classList.add("active");
+      document.getElementById(tab).classList.add("active");
+    });
+  });
 });
