60474833c1bdc90f0ad0582b8030dba9daab8ef9
Waraporn.SWSK <deerwaraporn2@gmail.com>
2026-04-10 11:28:56 +0200

    step: markers working before popup

--- src/scripts/map_page.js
+++ src/scripts/map_page.js
@@ -1,1 +1,21 @@
 import { map } from "../utils/map.js";
+
+import { getBaseUrl } from "../utils/api.js";
+
+const BASE_URL = getBaseUrl();
+
+async function loadPlants() {
+  const res = await fetch(`${BASE_URL}plants`);
+  const plants = await res.json();
+
+  console.log("PLANTS:", plants);
+
+  plants.forEach((plant) => {
+    const lat = 59.33;
+    const lng = 18.06;
+
+    L.marker([lat, lng]).addTo(map);
+  });
+}
+
+loadPlants();
--- src/utils/api.js
+++ src/utils/api.js
@@ -7,9 +7,20 @@
 //   return "https://your-backend.vercel.app/";
 // }
 
-export function getBaseUrl() {
-  if (window.location.hostname.includes("localhost") && false) { // Kommentera ut för att komma åt lokal server igen
-    return "http://localhost:3000/";
+/* export function getBaseUrl() {
+  if (window.location.hostname.includes("localhost") && false ) {  */
+    // Kommentera ut för att komma åt lokal server igen
+ /*    return "http://localhost:3000/";
   }
   return "https://plottwistgrupp11.vercel.app/"; 
+}
+ */
+export function getBaseUrl() {
+  const host = window.location.hostname;
+
+  if (host === "localhost" || host === "127.0.0.1") {
+    return "http://localhost:3000/";
+  }
+
+  return "https://plottwistgrupp11.vercel.app/";
 }
