32bf8f15fb4ec23ee60bb99e284adc51712b1313
emaniooo <eman.abubeker@yh.nackademin.se>
2026-04-20 22:53:04 +0200

    added navbar element selectors

---
+++ src/scripts/auth.js
@@ -0,0 +1,16 @@
+import { getBaseUrl } from "../utils/api.js";
+
+const BASE_URL = getBaseUrl();
+
+async function checkLoginStatus() {
+  const token = localStorage.getItem("token");
+
+  // Hämta navbar-element
+  const loginBtn = document.getElementById("nav-login-btn");
+  const profileIconContainer = document.getElementById("profile-icon-container");
+  const profileIcon = document.getElementById("profile-icon");
+  const profileDropdown = document.getElementById("profile-dropdown");
+  const notifLink = document.getElementById("notification-link");
+  const ddName = document.getElementById("dd-name");
+  const ddEmail = document.getElementById("dd-email");
+  }
